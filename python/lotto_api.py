d4aed2f5453e04266ab6cf98744a852a3a052066
bds01088 <bds01088@naver.com>
2022-01-14 17:37:54 +0900

    파이썬

---
+++ python/lotto_api.py
@@ -0,0 +1,13 @@
+import requests
+#requests 사용해서 로또 api에 데이터 요청하고 응답 받은 문서를 출력
+#날씨 api = https://www.metaweather.com/api/#locationsearch
+url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=997"
+#조회 요청/ 데이터 형태가 json이므로 json() 붙여야함
+#200을 피드백으로 받으면 형태가 달라서 뜨는 에러넘버인듯
+response = requests.get(url).json()
+
+#print(response)
+winners = []
+for i in range(1,7) :
+    winners.append(response[f'drwtNo{i}'])
+print(winners)
---
+++ python/lunch.py
@@ -0,0 +1,6 @@
+import random
+menu = ['짜장면', '짬뽕', '탕수육']
+
+choice = random.choice(menu)
+
+print(choice)
