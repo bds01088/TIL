5ac5130b592216d3e4c4ef6a4f38e0d23d93c283
김효근 <bds01088@naver.com>
2022-04-14 13:32:24 +0900

    22.04.14

--- python/age.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-n = input('당신의 영어이름은? : ')
-url = f"https://api.agify.io/?name={n}"
-
-response = requests.get(url).json()
-
-print(f'{n}의 나이는 {response["age"]}살 입니다.')
--- python/lunch.py
+++
@@ -1,6 +0,0 @@
-import random
-menu = ['짜장면', '짬뽕', '탕수육']
-
-choice = random.choice(menu)
-
-print(choice)
